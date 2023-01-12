0172e1f42f22ff2162f10c62bbfb3fa0eeb29d5f
Zalman Izak <thezalmanian@gmail.com>
2023-01-11 20:19:11 -0800

    Created Student and Advisor classes - Implemented most class fields

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a college advisor
+    /// </summary>
+    public class Advisor
+    {
+        /// <summary>
+        /// The advisor's legal first and last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The advisor's work email address
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The advisor's office location (building and room number)
+        /// </summary>
+        public string OfficeLocation { get; set; }
+    }
+}
---
+++ NestedObjects/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents an individual college student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// The student's legal first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The student's legal last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The student's date of birth (time is ignored)
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// The student's assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        /// <summary>
+        /// The student's mobile phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+    }
+}
